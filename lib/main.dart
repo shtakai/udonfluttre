61e9d391ff79e263671024867e3c885c59dbf703
Alyson Takai <shtakai@gmail.com>
2019-09-11 15:48:17 +0900

    Modify to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,42 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+    final title = 'Fxxkter title';
+    final message = 'Fxxkter message';
+
+    @override
+    Widget build(BuildContext context) {
+      return new MaterialApp(
+      title: 'fuck kill destroy',
+      home: new MyHomePage(
+        title: this.title,
+        message: this.message,
+      )
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  final String message;
+
+  MyHomePage({this.title, this.message}): super();
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'flutter demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('hell fuckerms'),
-        ),
-        body: Text(
-          'Hell fuck app fsskutter',
-          style: TextStyle(fontSize: 32.0),
-        )
-      )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Text(
+        widget.message,
+        style: TextStyle(fontSize: 38.0),
+      ),
     );
   }
 }
