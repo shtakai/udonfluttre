2998db349bcd35d224c3077ff9bdc1306ca6bee8
Alyson Takai <shtakai@gmail.com>
2019-09-11 17:33:19 +0900

    Use layout from flutterstudio

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
+void main() {
+  runApp(new MyApp());
+}
 class MyApp extends StatelessWidget {
-    final title = 'Fxxkter title';
 
-    @override
-    Widget build(BuildContext context) {
-      return new MaterialApp(
-      title: 'fuck kill destroy',
-      home: new MyHomePage(
-        title: this.title,
-      )
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Generated App',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF2196f3),
+        accentColor: const Color(0xFF2196f3),
+        canvasColor: const Color(0xFFfafafa),
+      ),
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({this.title}): super();
-
-  final String title;
-
+  MyHomePage({Key key}) : super(key: key);
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class Data {
-  int _price;
-  String _name;
-
-  Data(this._name, this._price): super();
-
-  @override
-  String toString() {
-    return _name + ':' + _price.toString() + '円';
-  }
-}
-
 class _MyHomePageState extends State<MyHomePage> {
-  static final _data = [
-    Data('Apple', 200),
-    Data('Orange', 150),
-    Data('Peach', 44)
-  ];
-  Data _item;
-
-  @override
-  void initState() {
-    super.initState();
-    _item = _data[0];
-  }
-
-  void _setData() {
-    setState(() {
-      _item = (_data..shuffle()).first;
-    });
-}
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('App Name'),
       ),
-
-      body: Text(
-        _item.toString(),
-        style: TextStyle(fontSize: 38.0),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _setData,
-          tooltip: 'set message',
-        child: Icon(Icons.star),
+      body:
+        Text(
+        "Suck em all",
+        style: new TextStyle(fontSize:32.0,
+            color: const Color(0xFF000000),
+            fontWeight: FontWeight.w700,
+            fontFamily: "Roboto"),
       ),
     );
   }
+
+  void fabPresses() {}
 }
