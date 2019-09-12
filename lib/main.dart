d29462a43a6eae080fc8edc37a216c3aa9e9d92e
Alyson Takai <shtakai@gmail.com>
2019-09-12 17:29:28 +0900

    Change to SliverAppBar/List;

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,60 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List _items = <Widget>[];
+
+  @override
+  void initState() {
+    super.initState();
+    for (var i = 0; i < 100; i++) {
+      var item = Container(
+        color: i.isOdd ? Colors.blue : Colors.white,
+        height: 100.0,
+        child: Center(
+          child: Text(
+            'No, $i',
+            style: const TextStyle(fontSize: 32.0),
+          ),
+        ),
+      );
+      _items.add(item);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Text(
-        widget.title,
-        style: TextStyle(fontSize: 38.0),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: true,
+            expandedHeight: 200.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: const Text('Sliver App Bar'),
+              background: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  Image.network(
+                    'https://www.rakuten.ne.jp/gold/rtm-select/wifi-rental/itempage/20_04_30d.jpg',
+                    fit: BoxFit.fill,
+                  ),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.android),
+                tooltip: 'icon button',
+                onPressed: () {
+                  print('pressed');
+                },
+              ),
+            ],
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(_items),
+          ),
+        ],
       ),
     );
   }
