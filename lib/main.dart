15228aade73cd08f5645dd87befef2f73b152972
Alyson Takai <shtakai@gmail.com>
2019-09-12 13:36:06 +0900

    Change to RaisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
           ),
-          FlatButton(
+          RaisedButton(
             key: null,
             onPressed: buttonPressed,
             color: Colors.black12,
