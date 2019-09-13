ae8f117db98e42012e0ca1ca9d08d5e04b36062f
Alyson Takai <shtakai@gmail.com>
2019-09-13 14:09:59 +0900

    Load text from asset file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'dart:async';
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,7 +29,7 @@ class FirstScreen extends StatefulWidget {
 
 class _FirstScreenState extends State<FirstScreen> {
   final _controller = TextEditingController();
-  final _fname = 'mydata.txt';
+  final _fname = 'assets/data.txt';
 
   @override
   Widget build(BuildContext context) {
@@ -51,76 +50,40 @@ class _FirstScreenState extends State<FirstScreen> {
           ),
           TextField(
             controller: _controller,
+            maxLines: 5,
             style: TextStyle(
               fontSize: 32.0,
             ),
           ),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            title: Text('save'),
-            icon: Icon(Icons.save),
-          ),
-          BottomNavigationBarItem(
-            title: Text('load'),
-            icon: Icon(Icons.open_in_new),
-          ),
-        ],
-        onTap: (int value) {
-          switch (value) {
-            case 0:
-              saveIt(_controller.text);
-              setState(() {
-                _controller.text = '';
-              });
-              showDialog(
-                context: context,
-                builder: (BuildContext context) => AlertDialog(
-                  title: Text('saved'),
-                  content: Text('save message to file'),
-                ),
-              );
-              break;
-            case 1:
-              loadIt().then((String value) {
-                setState(() {
-                  _controller.text = value;
-                });
-              });
-              showDialog(
-                context: context,
-                builder: (BuildContext context) => AlertDialog(
-                  title: Text('loaded'),
-                  content: Text('load message from file'),
-                ),
-              );
-              break;
-            default:
-              print('no default');
-          }
-        },
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.open_in_new),
+        onPressed: () => setState(() {
+          loadIt().then((String value) {
+            setState(() {
+              _controller.text = value;
+            });
+            showDialog(
+              context: context,
+              builder: (BuildContext context) => AlertDialog(
+                title: Text('loaded'),
+                content: Text('load message from asset'),
+              ),
+            );
+          });
+        }),
       ),
     );
   }
 
-  Future<File> getDataFile(String filemane) async {
-    final directory = await getApplicationDocumentsDirectory();
-    return File(directory.path + '/' + filemane);
-  }
-
-  void saveIt(String value) async {
-    getDataFile(_fname).then((File file) {
-      file.writeAsString(value);
-    });
+  Future<String> getDataAsset(String path) async {
+    return await rootBundle.loadString(path);
   }
 
   Future<String> loadIt() async {
     try {
-      final file = await getDataFile(_fname);
-      return file.readAsString();
+      return await getDataAsset(_fname);
     } catch (e) {
       return null;
     }
