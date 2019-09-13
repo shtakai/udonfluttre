027c5a611ab072f8345f74832b7aa205c904d2a1
Alyson Takai <shtakai@gmail.com>
2019-09-13 14:00:21 +0900

    Read/Save file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,33 +12,117 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        title: 'fuck kill destroy',
-        home: new MyHomePage(
-          title: this.title,
-        ));
+      title: 'fuck kill destroy',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => FirstScreen(),
+      },
+    );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  MyHomePage({this.title}) : super();
+class FirstScreen extends StatefulWidget {
+  FirstScreen({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _FirstScreenState createState() => new _FirstScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _FirstScreenState extends State<FirstScreen> {
+  final _controller = TextEditingController();
+  final _fname = 'mydata.txt';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('firstscreen'),
       ),
-      body: Text(
-        widget.title,
-        style: TextStyle(fontSize: 38.0),
+      body: Column(
+        children: <Widget>[
+          Text(
+            'Home screen',
+            style: TextStyle(
+              fontSize: 32.0,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+          ),
+          TextField(
+            controller: _controller,
+            style: TextStyle(
+              fontSize: 32.0,
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            title: Text('save'),
+            icon: Icon(Icons.save),
+          ),
+          BottomNavigationBarItem(
+            title: Text('load'),
+            icon: Icon(Icons.open_in_new),
+          ),
+        ],
+        onTap: (int value) {
+          switch (value) {
+            case 0:
+              saveIt(_controller.text);
+              setState(() {
+                _controller.text = '';
+              });
+              showDialog(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: Text('saved'),
+                  content: Text('save message to file'),
+                ),
+              );
+              break;
+            case 1:
+              loadIt().then((String value) {
+                setState(() {
+                  _controller.text = value;
+                });
+              });
+              showDialog(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: Text('loaded'),
+                  content: Text('load message from file'),
+                ),
+              );
+              break;
+            default:
+              print('no default');
+          }
+        },
       ),
     );
+  }
+
+  Future<File> getDataFile(String filemane) async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File(directory.path + '/' + filemane);
+  }
+
+  void saveIt(String value) async {
+    getDataFile(_fname).then((File file) {
+      file.writeAsString(value);
+    });
+  }
+
+  Future<String> loadIt() async {
+    try {
+      final file = await getDataFile(_fname);
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
   }
 }
