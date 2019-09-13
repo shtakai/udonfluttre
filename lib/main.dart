2bd9eeef4a1e5093c42cd8584c4e908578dbd0d3
Alyson Takai <shtakai@gmail.com>
2019-09-13 17:06:57 +0900

    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:html/dom.dart' as dom;
+import 'package:webfeed/webfeed.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,32 +13,229 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
         title: 'fuck kill destroy',
-        home: new MyHomePage(
-          title: this.title,
-        ));
+        theme: new ThemeData(
+          primarySwatch: Colors.pink,
+          primaryColor: const Color(0xFFe91e63),
+          accentColor: const Color(0xFFe91e63),
+          canvasColor: const Color(0xFFfafafa),
+        ),
+        home: new RssListPage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  final String title;
+class RssListPage extends StatelessWidget {
+  final List<String> names = ['主要ニュース', '国際情勢', '国内の出来事', 'IT関係'];
 
-  MyHomePage({this.title}) : super();
+  final List<String> links = [
+    'https://news.yahoo.co.jp/pickup/rss.xml',
+    'https://news.yahoo.co.jp/pickup/world/rss.xml',
+    'https://news.yahoo.co.jp/pickup/domestic/rss.xml',
+    'https://news.yahoo.co.jp/pickup/computer/rss.xml'
+  ];
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Fuckoo news checker'),
+        ),
+        body: Center(
+          child: ListView(
+            padding: EdgeInsets.all(10.0),
+            children: items(context),
+          ),
+        ));
+  }
+
+  List<Widget> items(BuildContext context) {
+    List<Widget> items = [];
+    for (var i = 0; 0 < names.length; i++) {
+      items.add(
+        ListTile(
+          contentPadding: EdgeInsets.all(10.0),
+          title: Text(
+            names[i],
+            style: TextStyle(fontSize: 24.0),
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => MyRssPage(
+                  title: names[i],
+                  url: links[i],
+                ),
+              ),
+            );
+          },
+        ),
+      );
+    }
+    return items;
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyRssPage extends StatefulWidget {
+  final String title;
+  final String url;
+
+  MyRssPage({@required this.title, @required this.url});
+
+  @override
+  _MyRssPageState createState() => new _MyRssPageState(title: title, url: url);
+}
+
+class _MyRssPageState extends State<MyRssPage> {
+  final String title;
+  final String url;
+
+  List<Widget> _items = <Widget>[];
+
+  _MyRssPageState({@required this.title, @required this.url}) {
+    getItems();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: Text(
-        widget.title,
-        style: TextStyle(fontSize: 38.0),
+      body: Center(
+        child: ListView(
+          padding: EdgeInsets.all(10.0),
+          children: _items,
+        ),
       ),
     );
+  }
+
+  void getItems() async {
+    List<Widget> list = <Widget>[];
+
+    Response res = await get(url);
+    RssFeed feed = RssFeed.parse(res.body);
+    for (RssItem item in feed.items) {
+      list.add(
+        ListTile(
+          contentPadding: EdgeInsets.all(10.0),
+          title: Text(
+            item.title,
+            style: TextStyle(
+              fontSize: 24.0,
+            ),
+          ),
+          subtitle: Text(
+            item.pubDate,
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => ItemDetailsPage(
+                  item: item,
+                  title: title,
+                  url: url,
+                ),
+              ),
+            );
+          },
+        ),
+      );
+    }
+
+    setState(() {
+      _items = list;
+    });
+  }
+}
+
+class ItemDetailsPage extends StatefulWidget {
+  final String title;
+  final String url;
+  final RssItem item;
+
+  ItemDetailsPage(
+      {@required this.item, @required this.title, @required this.url});
+
+  @override
+  _ItemDetails createState() => new _ItemDetails(item: item);
+}
+
+class _ItemDetails extends State<ItemDetailsPage> {
+  RssItem item;
+  Widget _widget = Text('wait...');
+
+  _ItemDetails({@required this.item});
+
+  @override
+  void initState() {
+    super.initState();
+    getItem();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(item.title),
+      ),
+      body: _widget,
+    );
+  }
+
+  void getItem() async {
+    Response res = await get(item.link);
+
+    dom.Document doc = dom.Document.html(res.body);
+    dom.Element hbody = doc.querySelector('.hbody');
+    dom.Element htotle = doc.querySelector('.newsTitle a');
+    dom.Element newslink = doc.querySelector('.newsLink');
+
+    print(newslink.attributes['href']);
+
+    setState(() {
+      _widget = SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  htotle.text,
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  hbody.text,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  child: Text(
+                    '続きを読む...',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  onPressed: () {
+                    launch(newslink.attributes['href']);
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    });
   }
 }
