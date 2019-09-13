c4b96ab914d6bad450fd64a0689529651d42e581
Alyson Takai <shtakai@gmail.com>
2019-09-13 11:15:38 +0900

    Add image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,50 +28,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: MyRenderBoxWidget(),
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/sauna.png'),
+        ),
       ),
     );
-  }
-}
-
-class MyRenderBoxWidget extends SingleChildRenderObjectWidget {
-  @override
-  RenderObject createRenderObject(BuildContext context) {
-    return _MyRenderBox();
-  }
-}
-
-class _MyRenderBox extends RenderBox {
-  @override
-  bool hitTest(HitTestResult result, {@required Offset position}) {
-    return true;
-  }
-
-  @override
-  void paint(PaintingContext context, Offset offset) {
-    Canvas c = context.canvas;
-
-    int dx = offset.dx.toInt();
-    int dy = offset.dy.toInt();
-
-    Paint p = Paint();
-    p.style = PaintingStyle.fill;
-    p.color = Color.fromARGB(150, 0, 200, 255);
-
-    Rect r = Rect.fromLTWH(dx + 150.0, dy + 50.0, 150.0, 150.0);
-
-    c.drawRect(r, p);
-    p.style = PaintingStyle.stroke;
-    p.color = Color.fromARGB(150, 200, 0, 255);
-    p.strokeWidth = 10.0;
-
-    r = Rect.fromLTWH(dx + 100.0, dy + 100.0, 150.0, 150.0);
-    c.drawRect(r, p);
   }
 }
