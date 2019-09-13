0b788fba2215f9133959103e1497692d4bc5fb97
Alyson Takai <shtakai@gmail.com>
2019-09-13 15:26:03 +0900

    Create Address note

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sql.dart';
+import 'package:collection/collection.dart';
+import 'package:path/path.dart';
+
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -8,33 +14,201 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        title: 'fuck kill destroy',
-        home: new MyHomePage(
-          title: this.title,
-        ));
+      title: 'fuck kill destroy',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => FirstScreen(),
+        '/list': (context) => SecondScreen(),
+      },
+    );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  MyHomePage({this.title}) : super();
+class FirstScreen extends StatefulWidget {
+  FirstScreen({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _FirstScreenState createState() => new _FirstScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _FirstScreenState extends State<FirstScreen> {
+  final _controllerA = TextEditingController();
+  final _controllerB = TextEditingController();
+  final _controllerC = TextEditingController();
+
+  final TextStyle styleA = TextStyle(
+    fontSize: 28.0,
+    color: Colors.black87,
+  );
+
+  final TextStyle styleB = TextStyle(
+    fontSize: 24.0,
+    color: Colors.black87,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Fuckemup home'),
       ),
-      body: Text(
-        widget.title,
-        style: TextStyle(fontSize: 38.0),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('Home screen'),
+            Text('Name', style: styleB),
+            TextField(
+              controller: _controllerA,
+              style: styleA,
+            ),
+            Text('Mail', style: styleB),
+            TextField(
+              controller: _controllerB,
+              style: styleA,
+            ),
+            Text('Tel', style: styleB),
+            TextField(
+              controller: _controllerC,
+              style: styleA,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            title: Text('add'),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text('list'),
+            icon: Icon(Icons.list),
+          ),
+        ],
+        onTap: (int index) {
+          if (index == 1) {
+            Navigator.pushNamed(context, '/list');
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save),
+        onPressed: () {
+          saveData();
+          showDialog(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: Text('saved'),
+              content: Text('inserted data into database'),
+            ),
+          );
+        },
       ),
     );
+  }
+
+  void saveData() async {
+    String dbPath = await getDatabasesPath();
+    String path = join(dbPath, 'mydatax.db');
+
+    String data1 = _controllerA.text;
+    String data2 = _controllerB.text;
+    String data3 = _controllerC.text;
+
+    String query =
+        'INSERT INTO mydata(name, mail, tel) VALUES("$data1", "$data2", "$data3")';
+
+    Database database = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute(
+            "CREATE TABLE IF NOT EXISTS mydata (id INTEGER PRIMARY KEY, name TEXT, mail TEXT, tel TEXT)");
+      },
+    );
+    await database.transaction((txn) async {
+      int id = await txn.rawInsert(query);
+      print('insert: $id');
+    });
+
+    setState(() {
+      _controllerA.text = '';
+      _controllerB.text = '';
+      _controllerC.text = '';
+    });
+  }
+}
+
+class SecondScreen extends StatefulWidget {
+  SecondScreen({Key key}) : super(key: key);
+
+  @override
+  _SecondScreenState createState() => new _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  List<Widget> _items = <Widget>[];
+
+  @override
+  void initState() {
+    super.initState();
+    getItems();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('List'),
+      ),
+      body: ListView(
+        children: _items,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            title: Text('add'),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text('list'),
+            icon: Icon(Icons.list),
+          ),
+        ],
+        onTap: (int index) {
+          if (index == 0) {
+            Navigator.pop(context);
+          }
+        },
+      ),
+    );
+  }
+
+  void getItems() async {
+    List<Widget> list = <Widget>[];
+    String dbPath = await getDatabasesPath();
+    String path = join(dbPath, 'mydatax.db');
+
+    Database database = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+      await db.execute(
+          "CREATE TABLE IF NOT EXISTS mydata (id INTEGER PRIMARY KEY, name TEXT, mail TEXT, tel: TEXT");
+    });
+
+    List<Map> result = await database.rawQuery('SELECT * FROM mydata');
+
+    for (Map item in result) {
+      list.add(ListTile(
+        title: Text(item['name']),
+        subtitle: Text(item['mail'] + ' ' + item['tel']),
+      ));
+    }
+
+    setState(() {
+      _items = list;
+    });
   }
 }
